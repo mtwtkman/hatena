b07363d160df56f2e806d80193ad3d9581cf5970
mtwtkman <punipuniomochi@gmail.com>
2015-05-09 13:45:22 +0900

    setup is benri

--- setup.py
+++ setup.py
@@ -1,17 +1,36 @@
 # -*- coding:utf-8 -*-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 __VERSION__ = '0.0.1'
+
+requirements = [
+    'flake8==2.4.0',
+    'mccabe==0.3',
+    'oauthlib==0.7.2',
+    'pep8==1.5.7',
+    'pyflakes==0.8.1',
+    'requests==2.7.0',
+    'requests-oauthlib==0.5.0',
+]
+
+extras_require = {
+    'example': [
+        'Beaker==1.7.0',
+        'bottle==0.12.8',
+    ]
+}
 
 params = {
     'name': 'hatena',
     'version': __VERSION__,
     'description': 'hatena API',
+    'install_requires': requirements,
+    'extras_require': extras_require,
     'author': 'mtwtk_man',
     'author_email': '',
     'url': 'http://github.com/mtwtkman/hatena/',
-    'packages': ['hatena'],
+    'packages': find_packages('hatena'),
     'license': 'WTFPL',
     'download_url': 'https://github.com/mtwtk_man/hatena/tarball/master',
 }
